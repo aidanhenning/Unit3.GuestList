f52496adbf6da645c30f3e59031072ea47619090
Aidan Henning <henningaidan@gmail.com>
2025-05-19 14:37:35 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useEffect } from "react";
 import { useState } from "react";
-import Home from "./components/Home/Home";
-import Details from "./components/Details/Details";
 
+import GuestDetails from "./components/GuestDetails/GuestDetails";
+import GuestList from "./components/GuestList/GuestList";
+
+/** Users can toggle between a list of guests and the details of a single guest */
 export default function App() {
-  const [data, setData] = useState([]);
   const [guestId, setGuestId] = useState(null);
-
-  useEffect(() => {
-    // 1. fetch the user data
-    // 1a. parse the data and find the part we want to store
-    // 2. set the users state variable to that data
-    fetch(
-      "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2504-FTB-ET-WEB-FT/guests"
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
-
   return (
     <main>
       {guestId ? (
-        <Details setGuestId={setGuestId} />
+        <GuestDetails guestId={guestId} setGuestId={setGuestId} />
       ) : (
-        <Home data={data} setGuestId={setGuestId} />
+        <GuestList setGuestId={setGuestId} />
       )}
     </main>
   );
